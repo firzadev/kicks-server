76b26f2e8e343f307fa04f3715f0b15986bfca27
firzadev <firzadev9@gmail.com>
2024-10-12 21:56:02 +0700

    add: product-schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { sql } from "drizzle-orm";
+import { index, unique } from "drizzle-orm/sqlite-core";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
@@ -28,12 +29,85 @@ export const products = sqliteTable("products", {
   ),
 });
 
-export const productVariants = sqliteTable("product_variants", {
+export const productVariants = sqliteTable(
+  "product_variants",
+  {
+    id: integer("id").primaryKey().notNull(),
+    productId: integer("product_id")
+      .references(() => products.id)
+      .notNull(),
+    color: text("color").notNull(),
+    size: integer("size").notNull(),
+    createdAt: text("created_at")
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+    updatedAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (table) => ({
+    productVariantIndex: index("product_id_variant_index").on(table.productId),
+  })
+);
+
+export const productStocks = sqliteTable(
+  "product_stocks",
+  {
+    id: integer("id").primaryKey().notNull(),
+    productVariantId: integer("product_variant_id")
+      .notNull()
+      .references(() => productVariants.id),
+    stock: integer("stock").notNull(),
+    createdAt: text("created_at")
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+    updatedAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (table) => ({
+    productVariantIndex: index("product_variant_id_index").on(
+      table.productVariantId
+    ),
+  })
+);
+
+export const categories = sqliteTable("categories", {
   id: integer("id").primaryKey().notNull(),
-  product_id: integer("product_id")
-    .references(() => products.id)
+  name: text("category").notNull(),
+  createdAt: text("created_at")
+    .default(sql`(CURRENT_TIMESTAMP)`)
     .notNull(),
+  updatedAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+    () => new Date()
+  ),
 });
+
+export const productCategories = sqliteTable(
+  "product_categories",
+  {
+    id: integer("id").primaryKey().notNull(),
+    productId: integer("product_id")
+      .notNull()
+      .references(() => products.id),
+    categoryId: integer("category_id")
+      .notNull()
+      .references(() => categories.id),
+    createdAt: text("created_at")
+      .default(sql`(CURRENT_TIMESTAMP)`)
+      .notNull(),
+    updatedAt: integer("updated_at", { mode: "timestamp" }).$onUpdate(
+      () => new Date()
+    ),
+  },
+  (table) => ({
+    uniqueProductCategory: unique().on(table.productId, table.categoryId),
+    productIndex: index("product_category_product_id_index").on(
+      table.productId
+    ),
+    categoryIndex: index("product_category_index").on(table.categoryId),
+  })
+);
 
 export type InsertUser = typeof users.$inferInsert;
 export type SelectUser = typeof users.$inferSelect;
